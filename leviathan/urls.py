54233775ab6a0861959f21f0a06014dc930890b6
fjzxl <fjzxl1@163.com>
2016-12-11 18:15:07 +0800

    Update urls.py

--- leviathan/urls.py
+++ leviathan/urls.py
@@ -16,7 +16,6 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 from . import views
-from django.conf import settings
 urlpatterns = [
     url(r'^$',views.index,name='index'),
     url(r'^users/',include('users.urls',namespace='user')),

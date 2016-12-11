b24f8cace9ea3f27bd2c88f9934d7dfc5daf987d
zxl <479851636@qq.com>
2016-12-12 01:37:44 +0800

    receptor

--- leviathan/settings.py
+++ leviathan/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = [u'127.0.0.1',u'localhost']
 # Application definition
 
 INSTALLED_APPS = [
+    'receptor.apps.ReceptorConfig',
     'publisher.apps.PublisherConfig',
     'users.apps.UsersConfig',
     'django.contrib.admin',
@@ -125,3 +126,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
--- receptor/forms.py
+++ receptor/forms.py
@@ -4,13 +4,13 @@ from django import forms
 
 # Forms
 class RegisterForm(forms.Form):
-    username = forms.CharField(min_length=4, label='用户名')
-    password = forms.CharField(min_length=4, label='密码',widget=forms.PasswordInput())
-    second_password = forms.CharField(min_length=4, label='再次输入密码',widget=forms.PasswordInput())
-    hospital=forms.IntegerField(label='医院id')
+    username = forms.CharField(min_length=4, label='用户名',widget=forms.TextInput(attrs={'class':"form-control form-control-solid placeholder-no-fix",'autocomplete':"off",'placeholder':"用户名",'name':"username",'type':"text"}))
+    password = forms.CharField(min_length=4, label='密码',widget=forms.TextInput(attrs={'class':"form-control form-control-solid placeholder-no-fix",'autocomplete':"off",'placeholder':"密码",'name':"password",'type':"password"}))
+    second_password = forms.CharField(min_length=4, label='再次输入密码',widget=forms.TextInput(attrs={'class':"form-control form-control-solid placeholder-no-fix",'autocomplete':"off",'placeholder':"再次输入密码",'name':"password",'type':"password"}))
+    hospital=forms.CharField(min_length=4,label='医院名',widget=forms.TextInput(attrs={'class':"form-control form-control-solid placeholder-no-fix",'autocomplete':"off",'placeholder':"用户名",'name':"username",'type':"text"}))
 
 
 
 class LoginForm(forms.Form):
-    username=forms.CharField(min_length=4, label='用户名')
-    password = forms.CharField(min_length=4, label='密码', widget=forms.PasswordInput())
+    username=forms.CharField( label='用户名',widget=forms.TextInput(attrs={'class':"form-control form-control-solid placeholder-no-fix",'autocomplete':"off",'placeholder':"用户名",'name':"username",'type':"text"}))
+    password = forms.CharField( label='用户名',widget=forms.TextInput(attrs={'class':"form-control form-control-solid placeholder-no-fix",'autocomplete':"off",'placeholder':"密码",'name':"password",'type':"password"}))
